fc0d9b9c7e732e991b4145494ece718b08176972
Chris Polak <chrisp2332@icloud.com>
2021-10-19 21:35:11 +1100

    added basic Linux support

--- zoom.sh
+++ zoom.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
-if [ "$#" -ne 0 ]
+
+func_help="\
+Usage:\n\
+  zoom [command] <arguments>\n\
+\n\
+Available commands:\n\
+  join - join a Zoom meeting with a specified meeting ID\n\
+  open (currently only on macOS) - open the zoom app\n\
+  -h, --help, help - display this help menu\
+"
+
+if [[ $(uname) == "Darwin" ]]
+then
+	open_link=open
+	open_app=$(open -a zoom.us)
+else
+	open_link=xdg-open
+	open_app=$(echo "zoom: open: sorry, currently this only supports macOS")
+fi
+
+if [[ $# != 0 ]]
 then
 	case "$1" in
 		join)
 			if [ -n "$2" ]
 			then
-				open "zoommtg://zoom.us/join?confno=$2"
+				$open_link "zoommtg://zoom.us/join?confno=$2"
 			else
 				echo "zoom: join: requires meeting ID specified" >&2
 			fi
 			;;
 		open)
-			open -a zoom.us ;;
-		-h|--help|help) printf "%s\n" \
-			"Usage:" \
-			"  zoom [command] <arguments>" \
-			"" \
-			"Available commands:" \
-			"  join - join a Zoom meeting with a specified meeting ID" \
-			"  open - open the zoom app" \
-			"  -h, --help, help - display this help menu"
-			;;
+			$open_cmd -a zoom.us ;;
+		-h|--help|help)
+			echo $func_help ;;
 		*)
 			echo "zoom: error: invalid command, see 'zoom -h' for a list of commands" >&2
 	esac
 else
-	printf "%s\n" \
-		"Usage:" \
-		"  zoom [command] <arguments>" \
-		"" \
-		"Available commands:" \
-		"  join - join a Zoom meeting with a specified meeting ID" \
-		"  open - open the zoom app" \
-		"  -h, --help, help - display this help menu"
+	echo $func_help
 fi
